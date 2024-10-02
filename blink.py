49911c84f1d794014231e9fe86edecdcfe2e287c
Felix Kunze <felix.kunze@t-online.de>
2024-10-02 14:02:42 +0200

    feat: guarantee to turn off led on close

--- blink.py
+++ blink.py
@@ -1,10 +1,32 @@
 from gpiozero import LED
 from time import sleep
+import signal
+import sys
 
 led = LED(21)
+running = True
 
-while True:
-    led.on()
-    sleep(1)
+def signal_handler(sig, frame):
+    global running
+    print("Signal received, preparing to shut down...")
+    running = False
+
+# Register the signal handler
+signal.signal(signal.SIGTERM, signal_handler)
+
+try:
+    while running:
+        led.on()
+        print("LED on")
+        sleep(1)
+        led.off()
+        print("LED off")
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("Program interrupted by user")
+finally:
     led.off()
-    sleep(1)
+    led.close()
+    print("Cleanup done. Exiting.")
+    sys.exit(0)
